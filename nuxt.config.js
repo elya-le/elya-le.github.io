05c776966cfd36d589ddc2aac1fdd8112a2cc8cf
Elya <elyaj.le@gmail.com>
2024-07-28 17:50:49 -0500

    update Nuxt configuration and index page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'elya.dev',
     htmlAttrs: {
@@ -15,27 +14,11 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  css: [],
+  plugins: [],
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  buildModules: [],
+  modules: [],
+  build: {},
+  compatibilityDate: '2024-07-28'
+};
--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,7 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'IndexPage'
-}
+<script setup>
 </script>
 
 <style scoped>
